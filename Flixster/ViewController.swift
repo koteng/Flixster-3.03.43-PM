61f6e78cbfa28e839e4ffd266f5ddebc756a2ab6
Kwaku Oteng <kwakuoteng@kwakus-macbook-air.local>
2019-01-30 12:38:32 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Kwaku Oteng on 1/30/19.
+//  Copyright © 2019 Kwaku Oteng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
